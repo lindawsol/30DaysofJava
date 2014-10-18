656f991d29edc1d37a73879540aa944943788efe
Linda Wang <lindawsol@users.noreply.github.com>
2014-10-17 19:36:35 -0500

    Day 11: Added another way to solve evenlySpaced.

--- evenlySpaced.java
+++ evenlySpaced.java
@@ -21,5 +21,29 @@ public class evenlySpaced {
         return ((Math.abs(a-b)== Math.abs(b-c)) || (Math.abs(a-c)==Math.abs(a-b)) ||( Math.abs(c-a)==Math.abs(b-c) || (Math.abs(a-c)==Math.abs(b-c))));
 
     }
+    public static boolean evenlySpaced2(int a, int b, int c){
+        int diff1 = 0;
+        int diff2 = 0;
+        int diff3 = 0;
+ 
+        if(a==b && a ==c)
+            return true;
+ 
+        if(a == b || b == c || a == c)
+            return false; 
+ 
+        diff1 = Math.abs(a - b); 
+        diff2 = Math.abs(a - c); 
+        diff3 = Math.abs(b - c); 
+ 
+        if(diff1 == diff2)
+            return true;
+        if(diff1 == diff3)
+            return true;
+        if(diff2 == diff3)
+            return true;
+ 
+    return false;
+    }
 
 }
